a9f9ec4d8003b9e3ef0129ff9cf9b8aa4a269b59
riya <riyashaji1@gmail.com>
2022-07-10 00:06:03 +0530

    added comments in the javascript.js file

--- javascript.js
+++ javascript.js
@@ -1,34 +1,32 @@
 const btn = document.getElementById('btn');
 
-    let email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    form_email = document.getElementById("email").value;
+let email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+form_email = document.getElementById("email").value;
 
-    btn.addEventListener('click', function onClick() {
-    btn.style.backgroundColor = 'gray';
-    if(email.test(form_email)!= true){
-            console.log('invalid email')
-        }
-        let d={}
-        let f = document.getElementById('form')
-        for(let i=0 ; i< f.length ; i++){
-            d[f.elements[i].name]=f.elements[i].value
-            
+btn.addEventListener('click', function onClick() {
+btn.style.backgroundColor = 'gray';                      //change button background colour
+                      
+if(email.test(form_email)!= true){                       //email checkup
+        console.log('invalid email')
+    } 
+    let d={}                           
+    let f = document.getElementById('form')              //for printing the form info to the console
+    for(let i=0 ; i< f.length ; i++){
+        d[f.elements[i].name]=f.elements[i].value
         }
         console.log(d)    
-    });
+});
     
-    
-  
-
- 
- function debounce(func, timeout = 2000){
+function debounce(func, timeout = 2000){                  //for the 2 second delay
   let timer;
   return (...args) => {
     clearTimeout(timer);
     timer = setTimeout(() => { func.apply(this, args); }, timeout);
   };
 }
+
 function saveInput(){
   console.log('Delay by 2 sec');
 }
+
 const processChange = debounce(() => saveInput());
